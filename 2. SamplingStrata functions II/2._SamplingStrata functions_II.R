dd3f1e28053c9aa3813fcbf28a9f1ff62becc7fa
unknown <gbarcaroli@gmail.com>
2019-05-13 16:10:44 +0200

    First version

---
+++ 2. SamplingStrata functions II/2._SamplingStrata functions_II.R
@@ -0,0 +1,87 @@
+## ---- include = F--------------------------------------------------------
+library(SamplingStrata)
+data("swissmunicipalities")
+data("swisserrors")
+data("swissframe")
+data("swissstrata")
+# load("2. SamplingStrata functions.RData")
+
+
+## ---- eval = T-----------------------------------------------------------
+data("swissmunicipalities")
+swissmunicipalities$id <- c(1:nrow(swissmunicipalities))
+swissmunicipalities$dom <- 1
+frame <- buildFrameDF(swissmunicipalities,
+                      id = "id",
+                      domainvalue = "REG",
+                      X = c("Surfacesbois","Surfacescult"),
+                      Y = c("Pop020", "Pop2040")
+)
+# choice of units to be selected in any case (census units)
+framecens <- frame[frame$X1 > 2500 
+                   | frame$X2 > 1200,]
+# remaining units 
+framesamp <- frame[!(frame$id %in% framecens$id),]
+
+
+
+## ---- eval = T-----------------------------------------------------------
+cv <- NULL
+cv$DOM <- "DOM1"
+cv$CV1 <- 0.1
+cv$CV2 <- 0.1
+cv <- as.data.frame(cv)
+cv <- cv[rep(row.names(cv),7),]
+cv$domainvalue <- c(1:7)
+cv
+
+
+## ---- eval = T-----------------------------------------------------------
+solution <- optimizeStrata2 (
+  errors = cv, 
+  framesamp = framesamp,
+  framecens = framecens, 
+  strcens = TRUE, 
+  alldomains = FALSE,
+  dom = 4,
+  iter = 50,
+  pops = 20,
+  nStrata = 5,
+  writeFiles = FALSE,
+  showPlot = FALSE,
+  parallel = FALSE
+)
+
+
+## ---- eval = T, echo=TRUE------------------------------------------------
+sum(round(solution$aggr_strata$SOLUZ))
+expected_CV(solution$aggr_strata)
+
+
+## ---- include=TRUE-------------------------------------------------------
+framenew <- solution$framenew
+table(framenew$LABEL)
+
+
+## ---- eval = T,echo=TRUE-------------------------------------------------
+strataStructure <- summaryStrata(solution$framenew,solution$aggr_strata,progress=TRUE)
+strataStructure
+
+
+## ---- eval = T,echo=TRUE-------------------------------------------------
+outstrata <- plotStrata2d(
+                  solution$framenew, 
+                  solution$aggr_strata,
+                  domain = 4, 
+                  vars = c("X1","X2"),
+                  labels =     c("Surfacesbois","Surfacescult")
+                  )
+
+
+## ---- eval = T,echo=TRUE-------------------------------------------------
+outstrata
+
+
+## ---- eval = T,echo=TRUE-------------------------------------------------
+samp <- selectSample(solution$framenew,solution$aggr_strata)
+
